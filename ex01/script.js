110ea0ed9ac75872795968ebf0deca1902addc44
Heloisa-Goncalvesz <helo.goncalves0515@gmail.com>
2026-04-01 08:49:07 -0300

    ajuste ex01

--- ex01/script.js
+++ ex01/script.js
@@ -1,4 +1,9 @@
 let numero  = Number(prompt("Insira um número qualquer:"));
 if (numero >0) {
-    alert ("Número positivo")
-}
+    alert ("Número positivo"); 
+} else if (numero === 0) {
+    alert ("Número neutro"); 
+    } else { 
+    alert ("Número negativo");
+    }
+
